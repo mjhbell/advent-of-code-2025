515360fac9ebf1ef5a4001de388c73fa2f79ab52
ripplecast <48491826+ripplecast@users.noreply.github.com>
2025-12-02 14:16:31 +0000

    Functional Part 1

--- 01/01-1.js
+++ 01/01-1.js
@@ -1,41 +1,58 @@
 const fs = require('node:fs');
 const readline = require('readline');
 
-//Globals to store column data
-var array1 = new Array();
-var array2 = new Array();
+let commands = new Array();
+const startPos = 50;
+const positionsCount = 100;
 
 //Stream reader to process input file
 var reader = readline.createInterface({
     input: fs.createReadStream('input.txt')
-    });
+});
 
-    //Push column values into their respective arrays
-    reader.on('line', function(line){
-        const arrLine = line.split('   ');
-            array1.push(arrLine[0]);
-                array2.push(arrLine[1]);
-                })
+//Push column values into their respective arrays
+reader.on('line', function (line) {
+    let direction = 1; //clockwise
+    if(line.substring(0,1) === 'L'){
+        direction = -1; //anticlockwise
+    };
+    let positions = parseInt(line.substring(1));
+    positions = positions % positionsCount;
+    if(positions < 0){
+        positions = positions * -1;
+    }
+    let command = {"direction":direction, "positions":positions};
+    commands.push(command);
+})
 
-                //Listen for end of read stream and start calculation.
-                reader.on('close', calculateAnswer);
+//Listen for end of read stream and start calculation.
+reader.on('close', calculateAnswer);
 
-                function calculateAnswer(){
-                    //Sort both arrays
-                        array1.sort();
-                            array2.sort();
+function calculateAnswer(){
 
-                                //Initialise answer variable
-                                    var answer = 0;
+    let zerocount = 0;
+    currentPos = startPos;
 
-                                        //Only calculate array length once.
-                                            const arrayLength = array1.length;
+    for(let i=0; i<commands.length; i++){
+        console.log(currentPos);
+        console.log(commands[i]);
+        if(commands[i].direction < 0){
+            currentPos = currentPos - commands[i].positions;
+        }else{
+            currentPos = currentPos + commands[i].positions;
+        }
+        if(currentPos < 0){
+            currentPos = 100+currentPos;
+        }
+        if(currentPos > 99){
+            currentPos = currentPos-100;
+        }
+        console.log(currentPos);
+        if(currentPos === 0){
+            zerocount += 1;
+        }
+    }
 
-                                                //Itterate through arrays compiling the answer
-                                                    for(var i=0; i<arrayLength; i++){
-                                                            answer += Math.abs(array1[i] - array2[i]);
-                                                                }
-
-                                                                    //Return the answer
-                                                                        console.log(`Answer: ${answer} , found in: ${Date.now()-start}ms`);
-                                                                        }
+    console.log(zerocount);
+    
+}
